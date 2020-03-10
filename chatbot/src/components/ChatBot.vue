d38e1fd767cffc549d290ed8ec4c2bf88ca8e666
Alex_Rogov <alexandre.rogov@gmail.com>
2020-03-09 22:23:30 -0700

    More data, takes in multiple words, scrollable div

--- chatbot/src/components/ChatBot.vue
+++ chatbot/src/components/ChatBot.vue
@@ -13,15 +13,14 @@
           </v-col>
         </v-row>
         <!--Messenging bubbles and text appears in this row-->
-          <v-row style="height: 400px;border: 1px solid #212121">
-            <v-col>
+          <v-row style="height: 400px;border: 1px solid #212121; overflow: auto;">
+            <v-col cols="12">
               <div id="chat-bot-response" class="chatbot">
                   <h2>*Chatbot*</h2>
                     <ul>
                         <!--Chatbot outputs are render into here-->
                     </ul>
               </div>
-                  
               <div id="user-response" class="user" style="text-align: right;">
                   <h2>*User*</h2>
                     <ul>
@@ -60,6 +59,7 @@
 </template>
 
 <script>
+/* eslint-disable */
   export default {
     name: 'Chatbot',
     data() {
@@ -76,7 +76,9 @@
             this.createNewElement('user-response', this.message);
             this.createEmptySpace('user-response');
             //TODO: add code for spliting words. Currently, it takes one key word
-            let chatBotResponse = this.hashQuery(this.message);
+            
+            let keywords = this.sentenceProcess(this.message);
+            let chatBotResponse = this.hashQuery(keywords);
 
             //TODO: if key is not found return             
             this.createEmptySpace('chat-bot-response');
@@ -90,11 +92,25 @@
       dest.appendChild(newBr);
     },
     //Create a new element into DOM
-    createNewElement: function(tagID, text) {
+    createNewElement: function(tagID, msg) {
             let newLi = document.createElement('li');
-            newLi.textContent = text;
+            newLi.textContent = msg;
             let dest = document.getElementById(tagID).getElementsByTagName('ul')[0];
             dest.appendChild(newLi);
+    },
+    sentenceProcess: function(msg) {
+      let substringArr = msg.split(' ');             //splits user's string into an array
+      substringArr.sort();                           //sort alphabetically
+      let strConcat = ''                             //Concatenated string
+      for(let i = 0; i < substringArr.length; i++) { //for each word, look up into hashtable
+        if(this.hashQuery(substringArr[i]) != undefined) {    //if not undefined, concatenate
+            strConcat = strConcat.concat(substringArr[i] + ' ');
+      console.log('split: ' + strConcat);
+        }      //  
+      }
+      strConcat = strConcat.trim();                         //trim ending space
+      console.log(strConcat);
+      return strConcat;
     },
     //Query through Hashtable
     hashQuery: function(keyword) {
@@ -170,9 +186,9 @@
             
             let ht = new HashTable(); 
             //TODO: Add data here
-            ht.add('agile', 'Agile is a lightwork framework for quick iterations of projects.')
+            ht.add('agile', 'Agile is a lightwork framework for quick iterations of sub-project. A very popular modern software development methodology')
             ht.add('fallwater', 'Double waterfall :)');
-            
+
             ht.add('Waterfall'.toLowerCase(),'Waterfall is a Software Development Life Cycle composed of phases that are based on each previous completed step.');
             ht.add('V-Shaped'.toLowerCase(),'V-shaped is a Software Development Life Cycle process where execuation is done in a "V" shape. In essence for every phase or step there is a directly associated testing phase.');
             ht.add('RAD'.toLowerCase(),'Rapid Application Development (RAD) has a quick turnaround time (~60 days) that heavily depends on code and screen generators, and other productivity tools. Users involved in all phases for feedback in planning and design.');
@@ -181,38 +197,51 @@
             ht.add('Spiral'.toLowerCase(),'Spiral is a SDLC that adoptes different SDLC such as incremental, waterfall, or evolutionary prototyping.');
             ht.add('Extreme Programming'.toLowerCase(),'Extreme Programming is a Software Development Life Cycle that accomodates change as a natural process and emphasizes values and principles rather than process');
             ht.add('XP'.toLowerCase(),'XP (Extreme Programming) is a Software Development Life Cycle that accomodates change as a natural process and emphasizes values and principles rather than process');
-            ht.add('Waterfall Benefits'.toLowerCase(),'The Waterfall SDLC is easy to plan since it has a linear-like development');
-            ht.add('Waterfall Drawbacks'.toLowerCase(),'The Waterfall SDLC needs well understood and stable requirements upfront');
-            ht.add('Waterfall features'.toLowerCase(),'Some features are linear development, easy to plan and staff, mistakes in previous phases are costly');
-            ht.add('V-Shaped Benefits'.toLowerCase(),'The V-Shaped SDLC is good for systems requiring high reliability because of the emphasis on verification and validation');
-            ht.add('V-Shaped Drawbacks'.toLowerCase(),'');
-            ht.add('V-Shaped Features'.toLowerCase(),'');
-            ht.add('RAD Benefits'.toLowerCase(),'The RAD SDLC has a quick turnaround time');
-            ht.add('RAD Drawbacks'.toLowerCase(),'');
-            ht.add('RAD Features'.toLowerCase(),'In the RAD SDLC, users are involved in all phases of development');
-            ht.add('Scrum Benefits'.toLowerCase(),'The beauty with scrum is its flexibility with requirement changes, does not require PM, and minimal managements.');
-            ht.add('Scrum Drawbacks'.toLowerCase(),'Some... troublesome.... issues are losing sight of scope creep,  difficult to schedule project completion, and difficult to integrate inexperience programmers. ');
-            ht.add('Scrum Features'.toLowerCase(),'Some features are focusing a tangible goal, and sharing daily issues');
-            ht.add('Incremental Benefits'.toLowerCase(),'Some cool benefits are creating working software early in development, easy to change requirements, and easier to test iterations');
-            ht.add('Incremental Drawbacks'.toLowerCase(),'T+C45he Incremental SDLC does not allow iterations');
-            ht.add('Incremental Features'.toLowerCase(),'Divide-and-conquer approach');
-            ht.add('Spiral Benefits'.toLowerCase(),'Takes advantage of strengths from waterfall, prototyping, and incremental SDLCs');
-            ht.add('Spiral Drawbacks'.toLowerCase(),'');
-            ht.add('Spiral Features'.toLowerCase(),'Focuses on risk analysis and management. Best for project where requirements are too complex or evolving');
-            ht.add('Extreme Programming Benefits'.toLowerCase(),'Extreme Programming embraces change and recognizes that all requirements will not be known at the beginning.');
-            ht.add('Extreme Programming Drawbacks'.toLowerCase(),'');
-            ht.add('Extreme Programming Features'.toLowerCase(),'Programming is done in pairs, work is completed at a pace that can be sustained indefinitely, test driven development which emphasizes customer involvement');
-            ht.add('XP Benefits'.toLowerCase(),'Extreme Programming embraces change and recognizes that all requirements will not be known at the beginning.');
-            ht.add('XP Drawbacks'.toLowerCase(),'');
-            ht.add('XP Features'.toLowerCase(),'Programming is done in pairs, work is completed at a pace that can be sustained indefinitely, test driven development which emphasizes customer involvement');
+            ht.add('Benefits Waterfall'.toLowerCase(),'The Waterfall SDLC is easy to plan since it has a linear-like development');
+            ht.add('Drawbacks Waterfall'.toLowerCase(),'The Waterfall SDLC needs well understood and stable requirements upfront');
+            ht.add('features Waterfall'.toLowerCase(),'Some features are linear development, easy to plan and staff, mistakes in previous phases are costly');
+            ht.add('Benefits V-Shaped'.toLowerCase(),'The V-Shaped SDLC is good for systems requiring high reliability because of the emphasis on verification and validation');
+            ht.add('Drawbacks V-Shaped'.toLowerCase(),'');
+            ht.add('Features V-Shaped'.toLowerCase(),'');
+            ht.add('Benefits RAD'.toLowerCase(),'The RAD SDLC has a quick turnaround time');
+            ht.add('Drawbacks RAD'.toLowerCase(),'');
+            ht.add('Features RAD'.toLowerCase(),'In the RAD SDLC, users are involved in all phases of development');
+            ht.add('Benefits Scrum'.toLowerCase(),'The beauty with scrum is its flexibility with requirement changes, does not require PM, and minimal managements.');
+            ht.add('Drawbacks Scrum'.toLowerCase(),'Some... troublesome.... issues are losing sight of scope creep,  difficult to schedule project completion, and difficult to integrate inexperience programmers. ');
+            ht.add('Features Scrum'.toLowerCase(),'Some features are focusing a tangible goal, and sharing daily issues');
+            ht.add('Benefits Incremental'.toLowerCase(),'Some cool benefits are creating working software early in development, easy to change requirements, and easier to test iterations');
+            ht.add('Drawbacks Incremental'.toLowerCase(),'T+C45he Incremental SDLC does not allow iterations');
+            ht.add('Features Incremental'.toLowerCase(),'Divide-and-conquer approach');
+            ht.add('Benefits Spiral'.toLowerCase(),'Takes advantage of strengths from waterfall, prototyping, and incremental SDLCs');
+            ht.add('Drawbacks Spiral'.toLowerCase(),'');
+            ht.add('Features Spiral'.toLowerCase(),'Focuses on risk analysis and management. Best for project where requirements are too complex or evolving');
+            ht.add('Benefits Extreme Programming '.toLowerCase(),'Extreme Programming embraces change and recognizes that all requirements will not be known at the beginning.');
+            ht.add('Drawbacks Extreme Programming '.toLowerCase(),'');
+            ht.add('Extreme Features Programming '.toLowerCase(),'Programming is done in pairs, work is completed at a pace that can be sustained indefinitely, test driven development which emphasizes customer involvement');
+            ht.add('Benefits XP '.toLowerCase(),'Extreme Programming embraces change and recognizes that all requirements will not be known at the beginning.');
+            ht.add('Drawbacks XP '.toLowerCase(),'');
+            ht.add('Features XP'.toLowerCase(),'Programming is done in pairs, work is completed at a pace that can be sustained indefinitely, test driven development which emphasizes customer involvement');
             ht.add('SDLC'.toLowerCase(),'Software Development Life Cycle development (SDLC) is a process to divide software development work to improve desing, product management, and project management');
-            ht.add('Hi'.toLowerCase(),'Hey, hi, hello');
-            ht.add('How are you'.toLowerCase(),'I am good....How are you?');
-            ht.add('Who are you'.toLowerCase(),'I am the all knowing, almighty Apurva Narayan');
-            ht.add('Like'.toLowerCase(),'Uhhh... hehexd... I like monkeys, OH hey, I like you :)');
+            // Conversational
+            ht.add('Hi'.toLowerCase(),'Hi!');
+            ht.add('Hey'.toLowerCase(),'Hey there!');
+            ht.add('Hello'.toLowerCase(),'Hello m8, ask me some SDLC questions!');
+            ht.add('Bye'.toLowerCase(),'Chao, till next time!');
+            ht.add('thank you'.toLowerCase(),'No, thank you :-)');
+            ht.add('thank'.toLowerCase(),'\"Thank you, Thank you, Thank you very much\" - me the chatbot');
+            ht.add('thanks'.toLowerCase(),'No problem m8 <3');
+
+            ht.add('are who you'.toLowerCase(),'I am the all knowing, almighty Prof. Apurva Narayan');
+            ht.add('are how you'.toLowerCase(),'I am good....How are you?');
+            ht.add('are','Are is for Arrrr. gimme that looty');
+            ht.add('how','How did you even question that phrase? I don\'t get it');
+            ht.add('you','Did you know, YOU are awesome :)');
+
+            ht.add('Like'.toLowerCase(),'..beep-boop.., OH hello there, I like you :)');
             ht.add('Love'.toLowerCase(),'I love COSC 310 :)');
             ht.add(''.toLowerCase(),'');
-            ht.add ('hehexd','hehexd indeed!');
+            ht.add('orc'.toLowerCase(),'ME ORC. ME ANGRY. RRRRrrrr');
+            ht.add('hehexd','hehe....xd ');
 
             //....add more hash elements\
             this.hashtable = ht;
